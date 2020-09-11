611b72a3d623566042f3b56ef51b123c91cba543
minhgit commit -am ud <ndinh725@gmail.com>
2020-09-11 17:18:44 +0700

    chuyen trang insert

--- index.php
+++ index.php
@@ -1,25 +1,10 @@
 <?php
 include_once("DB.php");
 $DB=new ConnectDB();
-if(isset($_POST['submit'])){
-
-    $MaSV=$_POST['MaSV'];
-    $HoSV=$_POST['HoSV'];
-    $TenSV=$_POST['TenSV'];
-    $Phai=$_POST['gender'];
-    $NgaySinh=$_POST['NgaySinh'];
-    $NoiSinh=$_POST['NoiSinh'];
-    $Khoa=$_POST['Khoa'];
-    $HocBong=$_POST['HocBong'];
-    $flag=$DB->insert($MaSV,$HoSV,$TenSV,$Phai,$NgaySinh,$NoiSinh,$Khoa,$HocBong);
-    if($flag){
-        echo "<script>alert('insert success!')</script>";
-        $result =$DB->select();
-        include ("index.html");
-        
-    }else{
-        echo "<script>alert(insert failure!)</script>";
-    }
-
+$result =$DB->select();
+if($result){
+    include("index.html");
+}else{
+    echo "<script>alert('No data to show')</script>";
 }
 ?>
--- insert.php
+++ insert.php
@@ -1,31 +1,25 @@
 <?php
-include("main.php");
-$servername="localhost";
-$username="root";
-$password="Huudinh98@";
-$dbname="QuanLyDiemSV";
-$MaSV=$_POST['MaSV'];
-$HoSV=$_POST['HoSV'];
-$TenSV=$_POST['TenSV'];
-$Phai=$_POST['gender'];
-$NgaySinh=$_POST['NgaySinh'];
-$NoiSinh=$_POST['NoiSinh'];
-$Khoa=$_POST['Khoa'];
-$HocBong=$_POST['HocBong'];
-$HocBong_f=floatval($HocBong);
-try{
-    $conn=new PDO("mysql:host=$servername;dbname=$dbname",$username,$password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-    $arr=[$MaSV, $HoSV, $TenSV, $Phai,$NgaySinh, $NoiSinh,$Khoa,$HocBong];
-    $sql="INSERT INTO DMSV (MaSV,HoSV,TenSV,Phai,NgaySinh,NoiSinh,MaKhoa,HocBong) values (?,?,?,?,?,?,?,?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->execute($arr);
-    $conn=null;
-    echo "insert success";
-    include("index.html");
+include_once("DB.php");
+$DB=new ConnectDB();
+if(isset($_POST['submit'])){
 
-}catch(PDOException $e){
-    echo $e->getMessage();
+    $MaSV=$_POST['MaSV'];
+    $HoSV=$_POST['HoSV'];
+    $TenSV=$_POST['TenSV'];
+    $Phai=$_POST['gender'];
+    $NgaySinh=$_POST['NgaySinh'];
+    $NoiSinh=$_POST['NoiSinh'];
+    $Khoa=$_POST['Khoa'];
+    $HocBong=$_POST['HocBong'];
+    $flag=$DB->insert($MaSV,$HoSV,$TenSV,$Phai,$NgaySinh,$NoiSinh,$Khoa,$HocBong);
+    if($flag){
+        echo "<script>alert('insert success!')</script>";
+        $result =$DB->select();
+        include ("index.html");
+        
+    }else{
+        echo "<script>alert(insert failure!)</script>";
+    }
+
 }
-
 ?>
